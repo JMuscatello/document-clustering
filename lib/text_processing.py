bd5007772ace232e458efa8564817622b62e1864
JMuscatello <jordan.muscatello@gmail.com>
2020-04-02 19:13:43 +0100

    Add stemmer class

---
+++ lib/text_processing.py
@@ -0,0 +1,67 @@
+import re
+import string
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def regex_preprocessor(text):
+    """
+    Removes numbers etc.
+
+    Args:
+        text (string): Raw text
+
+    Returns:
+        text with substitutions applied
+    """
+
+    text = re.sub(r'\([0-9]+\)', 'BRACKETNUM', text)
+    text = re.sub(r'[0-9]+\.', 'STOPNUM', text)
+    text = re.sub(r'[0-9]', 'd', text)
+    text = re.sub(r'\.(?=\w)', '. ', text)
+    text = re.sub(r'\:(?=\w)', ': ', text)
+    text = re.sub(r'\s+', ' ', text)
+
+    return text
+
+
+class StemmerTokenizer():
+    """
+    Class to stem (or lemmatize) text.
+
+    stem method returns list of stemmed or lemmatized tokens.
+
+    Args:
+        stemmer: Expects an instance of an nltk stemmer or lemmatizer.
+            Must implement a stem method that returns a list of strings
+    """
+
+    def __init__(self, stemmer):
+
+        self.stemmer = stemmer
+
+    def stem(self, text):
+        """
+        Removes stop words and punctuation and stems and tokenizes text.
+
+        Args:
+            text (string): Input text to tokenize
+
+        Returns:
+            list of stemmed/lemmatized tokens
+        """
+        punct_trans_table = str.maketrans(' ', ' ', string.punctuation)
+        stop_words = stopwords.words('english')
+
+        tokenized_text = word_tokenize(text)
+
+        text_wo_punctuation = [
+            token.translate(punct_trans_table) for token in tokenized_text]
+
+        filtered_text = [
+            self.stemmer.stem(token) for token in text_wo_punctuation
+            if token.lower() not in stop_words and
+            token != ' ' and token
+        ]
+    
+        return filtered_text
